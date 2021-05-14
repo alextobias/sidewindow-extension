76f5ba4d57a853848cdfefed2c02aaa47150bd99
Alex Tobias <alexanderleetobias@gmail.com>
2021-05-14 15:10:40 +0800

    Updated edit so that edits can happen in background, also fixed linting

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,12 @@
 import * as vscode from 'vscode';
 
 // importing io module from socketio client
-import { io, Socket } from 'socket.io-client'
+import { io, Socket } from 'socket.io-client';
 
 let extension_socket: Socket;
 let room_id: String
+let extensionSocket: Socket;
+let roomId: string;
 // let main_socket: undefined | Socket;
 
 // this method is called when your extension is activated
@@ -30,7 +32,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let startConnectionCommand = vscode.commands.registerCommand('sidewindow.startConnection', () => {
 		console.log("> Called startConnectionCommand");
 		// this will be taken away in production, since we will only be connecting to my server
-		console.log("> startConnectionCommand: Now prompting user for input")
+		console.log("> startConnectionCommand: Now prompting user for input");
 
 		const showInputBoxOptions = {
 			ignoreFocusOut: true,
@@ -39,23 +41,23 @@ export function activate(context: vscode.ExtensionContext) {
 			value: "https://sidewindow.herokuapp.com",
 			prompt: "server to connect to",
 			valueSelection: undefined
-		}
+		};
 		const connectionAddrInput = vscode.window.showInputBox(showInputBoxOptions);
 		connectionAddrInput.then((connectionAddr) => {
 			// determine what to do with input
 			// again, this will not be available in production
-			if(connectionAddr == undefined) {
+			if(connectionAddr === undefined) {
 				console.log("> startConnectionCommand: input box was closed.");
 				return;
 			}
-			else if(connectionAddr == "") {
+			else if(connectionAddr === "") {
 				console.log("> startConnectionCommand: empty connectionAddr. Returning.");
 				return;
 			}
 			else {
-				console.log(`> startConnectionCommand: attempting to connect to ${connectionAddr}`)
+				console.log(`> startConnectionCommand: attempting to connect to ${connectionAddr}`);
 
-				const client_soc_options = {
+				const extensionSocketOptions = {
 					reconnection: true,
 					reconnectionDelay: 1000,
 					reconnectionDelayMax: 5000,
@@ -63,67 +65,71 @@ export function activate(context: vscode.ExtensionContext) {
 					query: {
 						type: "extension",
 					}
-				}
+				};
 	
-				extension_socket = io(connectionAddr, client_soc_options);
+				extensionSocket = io(connectionAddr, extensionSocketOptions);
 				// main_socket = extension_socket;
 
-				if(extension_socket === undefined) {
-					console.log(`> startConnectionCommand: ERROR - connection failed mysteriously!`)
-					return
+				if(extensionSocket === undefined) {
+					console.log(`> startConnectionCommand: ERROR - connection failed mysteriously!`);
+					return;
 				} 
 				else {
 
 					console.log(`> startConnectionCommand: DEBUG - code is reached`);
 					// get room code from extension
 
-					extension_socket.on("connect", () => {
+					extensionSocket.on("connect", () => {
 						console.log(`> socket: connected to server at ${connectionAddr})`);
-						console.log(`> socket: this socket id is ${extension_socket.id}`)
-						room_id = extension_socket.id.slice(0, 4)
-						console.log(`> startConnectionCommand: room_id is ${room_id}`);
-						console.log(`> socket: connected to room ${room_id}`);
-						vscode.window.showInformationMessage(`sidewindow: Connected to room ${room_id}`, {modal: false});
+						console.log(`> socket: this socket id is ${extensionSocket.id}`);
+						roomId = extensionSocket.id.slice(0, 4);
+						console.log(`> startConnectionCommand: room_id is ${roomId}`);
+						console.log(`> socket: connected to room ${roomId}`);
+						vscode.window.showInformationMessage(`sidewindow: Connected to room ${roomId}`, {modal: false});
 
 						// TODO: update status bar item
-					})
+						initializeStatusBarItem(roomId);
+					});
 
-					extension_socket.on("disconnect", (msg) => {
+					extensionSocket.on("disconnect", (msg) => {
 						console.log(`> socket: disconnect event received! We've been disconnected`);
 						console.log(`> socket: disconnect message is ${msg}`);
 						vscode.window.showInformationMessage(`sidewindow: Disconnected.`, {modal: false});
-						extension_socket.disconnect()
-					})
+						extensionSocket.disconnect();
 
-					extension_socket.onAny((ev, arg) => {
+						// disposeStatusBarItem();
+						changeStatusBarItem("disconnected");
+					});
+
+					extensionSocket.onAny((ev, arg) => {
 						console.log(`> socket: received event ${ev} with args ${arg}`);
-					})
+					});
 
 					// the "source:event" format will be used in the future
-					extension_socket.on("browser:msg", (msg) => {
+					extensionSocket.on("browser:msg", (msg) => {
 						console.log(`> socket: received message '${msg} from a browser`);
 						vscode.window.showInformationMessage(`Received message: ${msg}`);
-					})
+					});
 
 					// for now, we are keeping the "source-event" format for compatibility
-					extension_socket.on("server-msg", (msg) => {
+					extensionSocket.on("server-msg", (msg) => {
 						console.log(`> socket: received message '${msg} from server`);
-					})
+					});
 
 				}
 			}
-		})
-	})
+		});
+	});
 
 	let sendMessageCommand = vscode.commands.registerCommand('sidewindow.sendMessage', () => {
 		console.log("> Called sendMessageCommand");
-		if(extension_socket === undefined) {
-			console.log("> sendMessageCommand: No socket currently active, returning.")
+		if(extensionSocket === undefined) {
+			console.log("> sendMessageCommand: No socket currently active, returning.");
 			vscode.window.showErrorMessage(`sidewindow: No socket currently active.`, {modal: false});
-			return
+			return;
 		}
 
-		console.log("> sendMessageCommand: Now prompting user for input")
+		console.log("> sendMessageCommand: Now prompting user for input");
 
 		const showInputBoxOptions = {
 			ignoreFocusOut: true,
@@ -132,28 +138,28 @@ export function activate(context: vscode.ExtensionContext) {
 			prompt: "Message to send to clients",
 			value: undefined,
 			valueSelection: undefined
-		}
+		};
 		const messageInput = vscode.window.showInputBox(showInputBoxOptions);
 		messageInput.then(
 			(message) => {
 				console.log(`> sendMessageCommand: user input '${message}'`);
-				extension_socket.emit("extension:msg", message);
+				extensionSocket.emit("extension:msg", message);
 				vscode.window.showInformationMessage(`sidewindow: Sent message '${message}' to connected clients`);
 			}
 		);
-	})
+	});
 
 	let shareFileCommand = vscode.commands.registerCommand('sidewindow.shareFile', function() {
 		console.log("> Called shareFileCommand");
 		const activeTextEditor = vscode.window.activeTextEditor;
-		if(extension_socket === undefined) {
+		if(extensionSocket === undefined) {
 			console.log(`> shareFileCommand: No socket currently active. Returning.`);
-			return
+			return;
 		}
 		else if(activeTextEditor === undefined) {
 			console.log(`> shareFileCommand: active text editor is undefined. Returning.`);
-			vscode.window.showErrorMessage(`sidewindow: No active editor to share.`)
-			return
+			vscode.window.showErrorMessage(`sidewindow: No active editor to share.`);
+			return;
 		}
 		// access active document and filepath
 		const activeDocument = activeTextEditor.document;
@@ -167,71 +173,93 @@ export function activate(context: vscode.ExtensionContext) {
 		console.log(`> shareFileCommand: document text is as follows:`);
 		console.log(documentText);
 
-		extension_socket.emit("extension:edits", documentText);
+		// broadcast current contents
+		extensionSocket.emit("extension:edits", documentText);
 
+		// set up event listener for when we make a change
 		console.log(`> shareFileCommand: attaching onDidChangeTextDocument event`);
 		vscode.workspace.onDidChangeTextDocument( changeEvent => {
+			// this event fires every time any document is changed, so we should check the document that got changed
 			console.log(`> extension: Got 'onDidChangeTextDocument' event`);
-			let text = activeDocument.getText();
-			extension_socket.emit("extension:edits", text);
+			if(changeEvent.document === activeDocument) {
+				console.log(`> extension: changed document matches selected shared activeDocument.`);
+				let text = activeDocument.getText();
+				extensionSocket.emit("extension:edits", text);
+			}
+			else {
+				console.log(`> extension: changed document doesn't matches shared activeDocument.`);
+			}
 		});
 
 		// set up event listener for browser edits
 		console.log(`> shareFileCommand: attaching browser:edits event`);
-		extension_socket.on("browser:edits", (msg) => {
+		extensionSocket.on("browser:edits", (msg) => {
 			console.log(`> socket: got [browser:edits] event aka new content from a browser client`);
 			// there's probably a better way to do this, but...
 			// need to access the properties of the document and its lines to get start and end positions
 			// then apply an edit to that range which replaces the document with the new received text
-			let lineCount = activeDocument.lineCount
+			let lineCount = activeDocument.lineCount;
 			// console.log("Document linecount: " + lineCount)
-			let firstLine = activeDocument.lineAt(0)
-			let lastLine = activeDocument.lineAt(lineCount - 1)
+			let firstLine = activeDocument.lineAt(0);
+			let lastLine = activeDocument.lineAt(lineCount - 1);
 			// console.log("Document first line:" + firstLine.text)
 			// console.log("Document last line:" + lastLine.text)
-			let firstLineRange = firstLine.range
-			let lastLineRange = lastLine.range
+			let firstLineRange = firstLine.range;
+			let lastLineRange = lastLine.range;
 			// console.log("First line range: " + firstLineRange.start + " to " + firstLineRange.end)
 			// console.log("Last line range: " + lastLineRange.start + " to " + lastLineRange.end)
-			let deleteRange = firstLineRange.with(firstLineRange.start, lastLineRange.end)
+			let deleteRange = firstLineRange.with(firstLineRange.start, lastLineRange.end);
 			// console.log("Delete range: " + deleteRange.start + " to " + deleteRange.end)
-			activeTextEditor.edit(editBuilder => {
-				editBuilder.delete(deleteRange)
-				editBuilder.insert(firstLineRange.start, msg)
-				// editBuilder deletes only work with ranges or selections
-				// so I have to give it the whole range of the document
-				// get document linecount
-				// let deleteRange = new Range(0,0, docum)
-				// editBuilder.delete()
-			})
-		})
-	})
+			// need to re-do activetexteditor if closed
+
+			// try this; it's a workspace edit
+			let newEdit = new vscode.WorkspaceEdit();
+			newEdit.replace(activeDocument.uri, deleteRange, msg);
+			vscode.workspace.applyEdit(newEdit);
+
+			// old way used the activeTextEditor.edit() method
+			// but the "activeTextEditor" closes upon changing tabs to another document
+			// it works when emitting changes since 'activeDocument' is preserved when changing tabs
+			// so when there's a change in the extension, we check that the change is coming from activeDocument
+			// but when we receive changes, we apply them to the activeTextEditor, which stops existing if we changed tabs
+
+			// activeTextEditor.edit(editBuilder => {
+			// 	editBuilder.delete(deleteRange)
+			// 	editBuilder.insert(firstLineRange.start, msg)
+			// 	// editBuilder deletes only work with ranges or selections
+			// 	// so I have to give it the whole range of the document
+			// 	// get document linecount
+			// 	// let deleteRange = new Range(0,0, docum)
+			// 	// editBuilder.delete()
+			// })
+		});
+	});
 
 	let disconnectCommand = vscode.commands.registerCommand('sidewindow.disconnect', () => {
 		console.log(`> Called disconnectCommand`);
-		if(extension_socket === undefined) {
+		if(extensionSocket === undefined) {
 			console.log(`> disconnectCommand: No socket currently active. Returning.`);
 			vscode.window.showErrorMessage(`sidewindow: No socket currently active.`, {modal: false});
-			return
+			return;
 		}
 		console.log(`> disconnectCommand: Now disconnecting socket...`);
-		extension_socket.disconnect()
+		extensionSocket.disconnect();
 		console.log(`> disconnectCommand: Socket disconnected.`);
 		// extension_socket = undefined;
 	});
 
 	let showRoomCodeCommand = vscode.commands.registerCommand('sidewindow.showRoomCode', () => {
 		console.log(`> Called showRoomCodeCommand`);
-		if(room_id === undefined) {
+		if(roomId === undefined) {
 			console.log(`> showRoomCommand: No socket currently active. Returning.`);
 			vscode.window.showErrorMessage(`sidewindow: No socket currently active.`, {modal: false});
-			return
+			return;
 		} else {
-			console.log(`> showRoomCodeCommand: showing room code ${room_id} to user.`);
-			vscode.window.showInformationMessage(`sidewindow: Room code is ${room_id}`, {modal: false});
-			return
+			console.log(`> showRoomCodeCommand: showing room code ${roomId} to user.`);
+			vscode.window.showInformationMessage(`sidewindow: Room code is ${roomId}`, {modal: false});
+			return;
 		}
-	})
+	});
 			
 
 	context.subscriptions.push(disposable);
