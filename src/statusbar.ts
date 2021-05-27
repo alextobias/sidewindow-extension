4437ae24d446cd05e6d12c62bf3126d00ff1ef29
Alex <alexanderleetobias@gmail.com>
2021-05-27 23:02:24 +0800

    Couldn't fix statusbar background change, but changed capitalization

--- src/statusbar.ts
+++ src/statusbar.ts
@@ -4,15 +4,16 @@ let statusBarItem: vscode.StatusBarItem;
 function initializeStatusBarItem(message: string) {
 	console.log(`> statusBarItem: initializing with text ${message}`);
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	statusBarItem.text = `sidewindow: ${message}`;
+	statusBarItem.text = `SideWindow: ${message}`;
 	statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.errorBackground');
+	// statusBarItem.backgroundColor = vscode.ThemeColor;
 	statusBarItem.color = vscode.ThemeColor;
 	statusBarItem.show();
 }
 
 function changeStatusBarItem(message: string) {
 	console.log(`> statusBarItem: changing message to ${message}`);
-	statusBarItem.text = `sidewindow: ${message}`;
+	statusBarItem.text = `SideWindow: ${message}`;
 	statusBarItem.show();
 }
 
