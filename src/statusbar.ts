ea8e2dfb1b7a957c2369434b3f967357deeab49e
Alex <alexanderleetobias@gmail.com>
2021-05-30 00:11:10 +0800

    Updated statusbar to use icon and theme color

--- src/statusbar.ts
+++ src/statusbar.ts
@@ -4,16 +4,16 @@ let statusBarItem: vscode.StatusBarItem;
 function initializeStatusBarItem(message: string) {
 	console.log(`> statusBarItem: initializing with text ${message}`);
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	statusBarItem.text = `SideWindow: ${message}`;
-	statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.errorBackground');
+	statusBarItem.text = `$(mirror-public) ${message}`;
+	// statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.errorBackground');
 	// statusBarItem.backgroundColor = vscode.ThemeColor;
-	statusBarItem.color = vscode.ThemeColor;
+	// statusBarItem.color = vscode.ThemeColor;
 	statusBarItem.show();
 }
 
 function changeStatusBarItem(message: string) {
 	console.log(`> statusBarItem: changing message to ${message}`);
-	statusBarItem.text = `SideWindow: ${message}`;
+	statusBarItem.text = `$(mirror-public) ${message}`;
 	statusBarItem.show();
 }
 
